5f6ff692fcb5e32b9cb8dcee94b6f7992f8ed9e0
Pallavi Lagisetti <pallavilagisetti2003@gmail.com>
2025-07-21 15:41:18 +0530

    Move Firebase config to .env and secure sensitive files

--- blog-bn/firebase/firebaseConfig.js
+++ blog-bn/firebase/firebaseConfig.js
@@ -1,12 +1,16 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import { createRequire } from 'module';
 
-dotenv.config();
+dotenv.config(); // Only needed locally
 
-
-const require = createRequire(import.meta.url);
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
